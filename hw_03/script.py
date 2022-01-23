742fb97a70f7c23476f235be310b8a4091e3fc7a
Pavel Shnyrin <shnirin.pavel@gmail.com>
2022-01-23 17:40:01 +0300

    Homework 3 first attempt. script.py to allocate books. Files with data and result also here

---
+++ hw_03/script.py
@@ -0,0 +1,44 @@
+import csv
+import json
+
+# read users from file into users
+with open("users.json", "r") as file_users:
+    users = json.load(file_users)
+
+# read books from file and store it to all_books
+all_books = []
+with open("books.csv", "r") as file_books:
+    books = csv.DictReader(file_books)
+
+    for book in books:
+        all_books.append(book)
+
+# setting up generator "books_gen"
+books_gen = (book for book in all_books)
+
+# list to store books for each user
+user_books = []
+
+# main part to allocate all books through the users
+while True:
+    try:
+        for user in users:
+            try:
+                # to check if the user already has any books and if not — add empty "books"
+                user_books = user["books"]
+            except KeyError:
+                user["books"] = []
+            # add next book from generator to current user's books
+            user_books.append(next(books_gen))
+            user["books"] = user_books
+            user_books = []
+
+    except StopIteration:
+        break
+
+# create new json users_with_books_json
+users_with_books_json = json.dumps(users, indent=4)
+
+# write it to file
+with open("users_with_books.json", "w") as file:
+    file.write(users_with_books_json)
