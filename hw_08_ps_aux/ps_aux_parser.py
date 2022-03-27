094a926bcf401ccb340185536d989a67a4d66ceb
Pavel Shnyrin <shnirin.pavel@gmail.com>
2022-03-27 12:16:21 +0300

    HW-08 ps aux parser

---
+++ hw_08_ps_aux/ps_aux_parser.py
@@ -0,0 +1,63 @@
+import csv
+from collections import defaultdict
+from datetime import datetime
+from subprocess import run, PIPE
+
+current_time = datetime.now()
+result_file = f"{current_time.strftime('%d-%m-%Y-%H:%M')}-scan"
+
+users = []
+process_counter = 0
+total_cpu = 0
+total_memory = 0
+processes_by_user = defaultdict(int)
+max_cpu_name = ""
+max_cpu = 0
+max_mem_name = ""
+max_mem = 0
+by_user_formatted = ""
+
+ps_aux = run(["ps", "aux"], stderr=PIPE, stdout=PIPE)
+ps_aux_output = ps_aux.stdout.decode("utf-8")
+
+header = ps_aux_output.splitlines()[0].replace("%", "").split()
+
+with open(f"{result_file}.csv", mode="w", encoding="UTF8") as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+
+    for line in ps_aux_output.splitlines()[1:]:
+        writer.writerow(line.split())
+
+with open(f"{result_file}.csv", mode="r") as f:
+    csv_reader = csv.DictReader(f)
+    for row in csv_reader:
+        total_cpu += float(row["CPU"])
+        total_memory += float(row["MEM"])
+        process_counter += 1
+        users.append(row["USER"])
+        processes_by_user[row["USER"]] += 1
+        if float(row["CPU"]) > max_cpu:
+            max_cpu = float(row["CPU"])
+            max_cpu_name = row["COMMAND"]
+        if float(row["MEM"]) > max_mem:
+            max_mem = float(row["MEM"])
+            max_mem_name = row["COMMAND"]
+
+for user, amount in sorted(processes_by_user.items(), key=lambda kv: kv[1], reverse=True):
+    by_user_formatted += f"{user}: {amount}\n"
+
+report = f"Отчёт о состоянии системы:\n" \
+         f"Пользователи системы: {', '.join(x for x in set(users))}\n" \
+         f"Процессов запущено: {process_counter}\n" \
+         f"Пользовательских процессов:\n" \
+         f"{by_user_formatted}" \
+         f"Всего памяти используется: {'{:.2f}'.format(total_memory)}\n" \
+         f"Всего CPU используется: {'{:.2f}'.format(total_cpu)}\n" \
+         f"Больше всего памяти использует: {max_mem_name[:20]}\n" \
+         f"Больше всего CPU использует: {max_cpu_name[:20]}"
+
+print(report)
+
+with open(f"{result_file}.txt", mode="w", encoding="UTF8") as f:
+    f.write(report)
