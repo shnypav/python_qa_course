3891bd823056007d099f5845ec3f8e6cd1c405b3
Pavel Shnyrin <pavel.shnyrin@jetbrains.com>
2024-05-08 14:16:23 +0200

    back to normal

---
+++ hw_02_figures/src/RectangleER.py
@@ -0,0 +1,24 @@
+from ..src.Figure import Figure
+
+
+class Rectangle(Figure):
+    name = "Rectangle"
+
+    def __init__(self, side_a, side_b):
+        if side_a < 0 or side_b < 0:
+            raise ValueError("Rectangle sides should be > 0")
+        self.side_a = side_a
+        self.side_b = side_b
+
+    @property
+    def perimeter(self):
+        return 2 * (self.side_a + self.side_b)
+
+    @property
+    def area(self) -> object:
+        """
+
+        @return:
+        """
+        area = self.side_a * self.side_b
+        return area
