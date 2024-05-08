75665b33036e92f5083cb39950fbd811e68e08d8
Pavel Shnyrin <pavel.shnyrin@jetbrains.com>
2024-05-08 14:08:55 +0200

    refac

--- hw_02_figures/src/RectangleE.py
+++ hw_02_figures/src/RectangleE.py
@@ -12,7 +12,7 @@ class Rectangle(Figure):
 
     @property
     def perimeter(self):
-        return 2 * (self.side_a + self.side_b)
+        return 2 * (self.side_a + self.side_b) * 2
 
     @property
     def area(self) -> object:
