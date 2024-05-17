81a43ad7bb0698044e86a12b35b96a7ccfdc1270
Pavel Shnyrin <pavel.shnyrin@jetbrains.com>
2024-05-17 11:17:56 +0200

    second change

--- hw_02_figures/src/Figure.py
+++ hw_02_figures/src/Figure.py
@@ -7,5 +7,6 @@ class Figure:
 
     def add_area(self, figure):
         if not isinstance(figure, Figure):
+            print("hello 2")
             raise ValueError("Could not calculate area with argument given")
         return self.area + figure.area
