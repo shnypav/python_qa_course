9d6a4950db9d83b3e0c86d9e5a98b9b5cdb156f2
Pavel Shnyrin <pavel.shnyrin@jetbrains.com>
2024-05-21 13:24:43 +0200

    aaa

--- hw_02_figures/src/Rectangle.py
+++ hw_02_figures/src/Rectangle.py
@@ -12,7 +12,7 @@ class Rectangle(Figure):
 
     @property
     def perimeter(self):
-        print("perimeter sfsdfsdfs")
+        print("perimeter sfsdfsdfddds")
         return 2 * (self.side_a + self.side_b) * 2
 
     @property
